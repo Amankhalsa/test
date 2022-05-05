117324f48e8363018db64faa36db866eadd15452
Amankhalsa <66160544+Amankhalsa@users.noreply.github.com>
2022-05-05 22:57:05 +0530

    Add files via upload

---
+++ basket_modal.php
@@ -0,0 +1,28 @@
+  <div style="position: fixed;display:none; transition: none 0s ease 0s; background: rgb(255, 255, 255); padding-right: 17px; display: block;overflow: hidden ;"
+    class="modal show" id="startModal" role="dialog" aria-modal="true">
+    <div style="position:unset;background:#fff;" class="modal-dialog">
+ 
+        <div style="position:absolute;top:0;left:0;bottom:0;background:#fff;" class="modal-content">
+            <!--<div class="modal-header">-->
+         
+            <!--</div>-->
+            <div class="modal-body">
+                <div class="row  ">
+                    <div class="col-md-12 text-center" >
+                        <a href="https://www.svprinterworld.com/setup-norton/" target="_blank">
+                        <img src="Printer_basket_Model.webp" class="img-fluid">
+                        </a>
+                    </div>
+        
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script>
+    $(document).ready(function () {
+        $('#startModal').show();
+    })
+</script>
